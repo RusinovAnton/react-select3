0bb886e97d5c8f224a9ba632558815446ecc94bf
Rusinov <rusinov.amgrade@gmail.com>
2016-12-11 22:41:44 +0200

    bump

--- src/components/SelectionArrow.js
+++ src/components/SelectionArrow.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
 
-const SelectionArrow = () => (<span className="react-select-selection__arrow" role="presentation"><i/></span>)
+const SelectionArrow = () => (<span className="react-select-selection__arrow" role="presentation"><b/></span>)
 
 export default SelectionArrow
