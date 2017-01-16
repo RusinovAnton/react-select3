0a92974654092eb122b3c2956e4205e7d827bba3
Rusinov Anton <rusinovantondev@gmail.com>
2017-01-16 23:12:10 +0200

    polish build

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,44 @@
 const babel = require('gulp-babel');
+const del = require('del');
 const eslint = require('gulp-eslint');
 const gulp = require('gulp');
-const jsdoc = require('gulp-jsdoc3');
 const react = require('gulp-react');
+const runSequence = require('run-sequence');
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 
 
 gulp.task('styles', () => {
-    return gulp.src('./src/styles.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./dist/'))
-})
-
-gulp.task('docs', (cb) => {
-    return gulp.src(['README.md', './dist/**/*.js'], { read: false })
-        .pipe(jsdoc(cb))
+  return gulp.src('./src/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/'))
 })
 
 gulp.task('transform', () => {
-    return gulp.src('./src/**/*.js')
-        .pipe(babel())
-        .pipe(react({ harmony: false, es6module: true }))
-        .pipe(gulp.dest('dist/'));
+  return gulp.src('./src/**/*.js')
+    .pipe(babel())
+    .pipe(react({ harmony: false, es6module: true }))
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('lint', () => {
-    gulp.src(['./src/**/*.js', '!node_modules/**'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+  gulp.src(['./src/**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
-gulp.task('dist', ['transform', 'styles', 'lint']);
-
-gulp.task('watch', ['styles', 'transform'], () => {
-    gulp.watch('./src/styles.scss', ['styles'])
-    gulp.watch('./src/**/*.js', ['transform'])
+gulp.task('clean', () => {
+  del.sync('./dist')
 })
 
-gulp.task('default', ['transform']);
+gulp.task('dist', (done) => {
+  runSequence('clean', 'transform', 'styles', 'lint', done)
+});
+
+gulp.task('watch', ['styles', 'transform'], () => {
+  gulp.watch('./src/styles.scss', ['styles'])
+  gulp.watch('./src/**/*.js', ['transform'])
+})
+
+gulp.task('default', ['dist']);
