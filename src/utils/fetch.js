888ccadd82cfc84591da558c1b1c5c28a031e233
Rusinov Anton <rusinovantondev@gmail.com>
2017-01-22 19:48:08 +0200

    fix eslint errors

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -8,24 +8,23 @@ export default (endpoint) => {
     credentials: 'same-origin',
   })
     .then(response => {
-        let data = []
+      let data = []
 
-        if (response.ok) {
-          try {
-            data = response.json()
-          } catch (err) {
-            console.warn(err) // eslint-disable-line no-console
-          }
-        } else if (response.status === 404) {
-          return []
-        } else {
-          throw new Error('Server error')
+      if (response.ok) {
+        try {
+          data = response.json()
+        } catch (err) {
+          console.warn(err) // eslint-disable-line no-console
         }
-
-        return data
-      },
-      err => {
-        console.warn(err) // eslint-disable-line no-console
+      } else if (response.status === 404) {
+        return []
+      } else {
+        throw new Error('Server error')
       }
-    )
+
+      return data
+    },
+    err => {
+      console.warn(err) // eslint-disable-line no-console
+    })
 }
--- src/utils/makeString.js
+++ src/utils/makeString.js
@@ -1,3 +1,3 @@
 import isNil from 'lodash/isNil'
 
-export default val => isNil(val) ? val : String(val)
+export default val => isNil(val) ? val : String(val) // eslint-disable-line no-confusing-arrow
