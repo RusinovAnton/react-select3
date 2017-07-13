92e48494cba0d5f024bf94d2dc5c4523f4fd5fe3
Rusinov Anton <rusinovantondev@gmail.com>
2017-07-13 13:01:55 +0300

    refactor eslint

--- src/components/SelectError.js
+++ src/components/SelectError.js
@@ -2,9 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 
-const SelectError = ({ error }, { cssClassNamePrefix }) => (typeof error === 'string') ?
-  (<span className={`${cssClassNamePrefix}__error`}>{ error }</span>)
-  : null;
+const SelectError = ({ error }, { cssClassNamePrefix }) => {
+  return (typeof error === 'string') ?
+    (<span className={`${cssClassNamePrefix}__error`}>{ error }</span>)
+    : null;
+};
 
 SelectError.contextTypes = {
   cssClassNamePrefix: PropTypes.string,
